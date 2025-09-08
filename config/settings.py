4bcdef1475df5476fdbe8483a5070ea90301af93
Filipe Marques Hottis <lipehottis@gmail.com>
2025-09-08 16:50:53 -0300

    feat: User update page is working

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,25 @@ TEMPLATES = [
     },
 ]
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,  # 👈 mínimo de caracteres
+        }
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
 # Static files (CSS, JS, imagens)
 STATIC_URL = '/static/'
 
--- core/views/user_profile.py
+++ core/views/user_profile.py
@@ -1,8 +1,48 @@
-from django.http import HttpRequest
-from django.shortcuts import render
+from typing import cast
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from ..models.user import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+from django.contrib import messages
 
 
 @login_required
 def user_profile(request: HttpRequest):
-    return render(request, "user_profile.html", {"user": request.user})
+    user = cast(User, request.user)
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+
+        if username:
+            user.username = username
+        if email:
+            user.email = email
+
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password:
+            if password == confirm_password:
+                try:
+                    validate_password(password, user)  # 🚨 Valida aqui
+                    user.set_password(password)
+                except ValidationError as e:
+                    messages.error(
+                        request,
+                        "Erro na senha: " + "; ".join(e.messages)
+                    )
+                    return render(request, "user_profile.html", {
+                        "user": user,
+                    })
+
+            else:
+                messages.error(request, "As senhas não coincidem.")
+                return render(request, "user_profile.html", {"user": user})
+
+        user.save()
+        return redirect("user_profile")
+
+    return render(request, "user_profile.html", {"user": user})
